2a7f6a6afa1bb6f6639b34d2157a106e678fd59e
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-19 20:09:10 -0700

    Update index.js
    create store, add reducer, middleware, and devtool extensions using compose.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware} from 'redux';
+//compose combines middleWares
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
 //where redux lives 
 
-//1.set up store 
+//enable Redux DevTools Extension 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//storing global data, sending action object -> reducers decide what to update -> return new store
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 //
 
 ReactDOM.render(
+  //strict mode highlights potential problems in application 
   <React.StrictMode>
-    <App />
+    {/* Provider component makes Redux store available to any nested components that need access to the Redux store. Here we are providing store as a prop.  */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
