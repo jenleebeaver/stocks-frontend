45ef8d0fca84732aa1b011c118d708a2d4a47b9a
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-21 09:20:14 -0700

    Create fetchPortfolios.js

---
+++ src/actions/fetchPortfolios.js
@@ -0,0 +1,18 @@
+
+//!check out some learn lessons examples
+export function fetchPortfolios(action){
+    //   fetch('http://localhost:3000/api/v1/portfolios', {
+    //   method: 'GET'
+    // })
+    // //promise - response from request
+    // //return => arg
+    // .then(response => response.json())
+    // .then(portfoliodata => 
+    //   //update redux store
+    //   console.log(portfoliodata))
+
+    return action
+}
+
+//dispatching action object. action -> reducer which returns new version of state based on action 
+// dispatch(action)
