dac23fabda2ee074fb506ebc83dfa9f9a3f4e664
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-24 10:28:35 -0700

    Update fetchPortfolios.js
    add dispatch

--- src/actions/fetchPortfolios.js
+++ src/actions/fetchPortfolios.js
@@ -1,17 +1,22 @@
 
 //!check out some learn lessons examples
 export function fetchPortfolios(action){
-    //   fetch('http://localhost:3000/api/v1/portfolios', {
-    //   method: 'GET'
-    // })
-    // //promise - response from request
-    // //return => arg
-    // .then(response => response.json())
-    // .then(portfoliodata => 
-    //   //update redux store
-    //   console.log(portfoliodata))
-
-    return action
+    console.log('inside fetch portfolios.')
+    return (dispatch) => {
+      fetch('http://localhost:3000/api/v1/portfolios', {
+      method: 'GET'
+        })
+        //promise - response from request
+        //return => arg
+        .then(response => response.json())
+        .then(portfolios => 
+        //update redux store
+        //dispatch action object with a type and payload and update redux store 
+        dispatch({
+            type: 'FETCH_PORTFOLIOS',
+            payload: portfolios
+        }))
+    }
 }
 
 //dispatching action object. action -> reducer which returns new version of state based on action 
