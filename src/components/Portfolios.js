99d7e0c1c3a124defda6524874ab4360ef5f8bba
jenleebeaver <jen.lee.beaver@gmail.com>
2021-05-24 09:58:53 -0700

    boilerplate code

---
+++ src/components/Portfolios.js
@@ -0,0 +1,15 @@
+//List of Portfolios
+import React from 'react';
+
+//functional. props need to be passed in as an arg unlike class components where we can call this.props 
+//can also pass in specific props if neccessary 
+const Portfolios = (props) => {
+    return (
+        <div>
+            Portfolios
+        </div>
+    )
+}
+
+export default Portfolios
+
