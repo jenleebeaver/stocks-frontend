e90f84473b9129228480496d54c1d496aeb55822
jenleebeaver <jen.lee.beaver@gmail.com>
2021-06-08 15:33:28 -0700

    Update searchStockPrices.js
    add css

--- src/components/searchStockPrices.js
+++ src/components/searchStockPrices.js
@@ -20,8 +20,10 @@ export default class SearchStockPricesContainer extends Component {
 
         const value = this.state.searchSymbol;
 
+        console.log(value)
         //Quote
         finnhubClient.quote(value, (error, data, response)     => {
+            console.log(error)
             this.setState({ prices: data })
             console.log(data)
         });    
@@ -30,21 +32,30 @@ export default class SearchStockPricesContainer extends Component {
     render() {
         return (
             <div className="search-prices">
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit}
+                    className="max-w-xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6 rounded-xl font-semibold">
                     <label htmlFor="search-input">
-                        <h4>Search Current Stock Price</h4>
+                        <h4 className="text-center text-l">
+                            Search Current Stock Price
+                        </h4>
                     </label>
                     <input id="search-input"
                         type="text"
                         placeholder="Search Current Stock Price Using Symbol"
-                        style={{ width: 300}}
                         value={this.state.searchSymbol}
+                        name="symbol-input"
+                        className="text-center w-full border p-4 my-4"
                         onChange={this.handleSearchInputChange}/>
                     <br/>
                     <br />
-                    <button type="submit">Submit</button>
+                    <button type="submit"
+                        className="w-full p-3 bg-purple-300 hover:bg-purple-400 transition-all duration-200 rounded-xl">
+                            Submit
+                    </button>
                 </form>
                 {typeof this.state.prices === 'object' && this.state.prices.length > 0 && <h4> Current Stock Price By Search</h4>}
+                <br/>
+                <br/>
                 <Prices prices={this.state.prices}/>
             </div>
         )
