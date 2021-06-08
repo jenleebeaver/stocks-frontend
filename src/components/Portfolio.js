7c5bfe02a0f2f321783463e309fd159df288a139
jenleebeaver <jen.lee.beaver@gmail.com>
2021-06-08 12:44:12 -0700

    Update Portfolio.js
    take out <li> styling

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import SearchStockPrices from '../components/searchStockPrices.js';
+// import SearchStockPrices from '../components/searchStockPrices.js';
 
 const Portfolio = ({ portfolio }) => {
-    return (
-        <li className="" key={portfolio.id}> 
-            <h3>
+    return ( 
+        <div>
+            <h3 className="" key={portfolio.id}>
                 <u>{portfolio.portfolio_name}</u>
             </h3> 
             {/* <PortfoliosTable portfolios={portfolio}/>  */}
-            <SearchStockPrices />
-        </li>       
+            {/* <SearchStockPrices /> */}  
+        </div>    
     )
 }
 
