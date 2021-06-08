669c8ffa1f2dc808b89cb352cf2677ec0215a703
jenleebeaver <jen.lee.beaver@gmail.com>
2021-06-08 15:32:22 -0700

    Update finnhub_api.js
    add process.env to api key

--- src/actions/finnhub_api.js
+++ src/actions/finnhub_api.js
@@ -3,7 +3,7 @@
 const finnhub = require('finnhub');
 
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = REACT_APP_FINNHUB_API_KEY;
+api_key.apiKey = process.env.REACT_APP_FINNHUB_API_KEY;
 
 const finnhubClient = new finnhub.DefaultApi();
 
