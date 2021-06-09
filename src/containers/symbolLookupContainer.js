e63ef476a16c102c3e03306bd7ea6da3dd7243b6
jenleebeaver <jen.lee.beaver@gmail.com>
2021-06-08 17:54:45 -0700

    Create symbolLookupContainer.js

---
+++ src/containers/symbolLookupContainer.js
@@ -0,0 +1,63 @@
+  
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import StockSymbol from '../components/StockSymbol';
+
+function getSearch(searchTerm) {
+    const BASE_URL =
+    `https://finnhub.io/api/v1/search?q=${searchTerm}&token=${process.env.REACT_APP_FINNHUB_API_KEY}`
+
+    return BASE_URL
+}
+
+
+class SymbolLookupContainer extends Component {
+
+    state = {
+      searchTerm: '',
+      results: []
+    };
+  
+    handleSearchInputChange = event =>
+      this.setState({ searchTerm: event.target.value });
+  
+    handleSubmit = event => {
+      event.preventDefault();
+  
+      fetch(getSearch(this.state.searchTerm))
+        .then(res => res.json())
+        .then(res => this.setState({ results: res.result }));
+    };
+  
+    render() {
+      return (
+        <div >
+          <form onSubmit={this.handleSubmit}
+            className="max-w-xl w-3/4 mx-auto mt-16 shadow-lg px-4 py-6 rounded-xl font-semibold">
+            <label htmlFor="search-input"
+                className="text-center text-l">
+                    Search For Stock Symbol Using Symbol, Name, Isin, or Cusip
+            </label>
+            <input
+              id="search-input"
+              type="text"
+              name="stock_symbol_input"
+              className="text-center w-full border p-4 my-4"
+              onChange={this.handleSearchInputChange}
+            />
+            <button className="w-full p-3 bg-purple-300 hover:bg-purple-400 transition-all duration-200 rounded-xl"
+            type="submit">
+                Submit
+            </button>
+          </form>
+       
+         <div className="grid md:grid-flow-col grid-cols-3 grid-rows-3 gap-4">
+          {this.state.results.map(entry => <StockSymbol data={entry} />)}
+         </div>
+        </div>
+      );
+    }
+  }
+  
+  export default SymbolLookupContainer;
+  
