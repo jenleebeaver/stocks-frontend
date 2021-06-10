88142a1c34d82b6902bf48188c042bc7e18957c6
jenleebeaver <jen.lee.beaver@gmail.com>
2021-06-10 09:57:01 -0700

    Update Prices.js
    delete extraneous code

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -15,33 +15,8 @@ const Price = ({
                 <p className="text-center">{prices.c}</p>
                 <br/>
                 <PricesTable prices={prices}/>
-            {/* use shape for company Profile 2 data */}
-            {/* <header>
-                <a className="stock-link"
-                    href="">
-                        {symbol}
-                </a>
-                <br/>
-                <span className="displaySymbol">
-                    {displaySymbol}
-                </span>
-            </header>
-            <blockquote>{description}</blockquote> */}
         </div>
     )
 }
-
-// const Stocks = ({ stocks }) => {
-//     return (
-//     <div className="stock-list">
-//         {stocks.map(Stock)}
-//     </div>
-//     )
-// }
-
-
-// Stocks.defaultProps = {
-//     stocks: []
-// };
 
 export default Price;
