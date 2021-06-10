418f5dcfafa21139b3cf6ba2e2536e5b11fd912a
jenleebeaver <jen.lee.beaver@gmail.com>
2021-06-10 14:17:44 -0700

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,13 @@
 import React from 'react'
-// import SearchStockPrices from '../components/searchStockPrices.js';
+import Watchlist from './Watchlist.js'
 
-const Portfolio = ({ portfolio }) => {
+const Portfolio = ({ watchlist }) => {
+   
     return ( 
         <div>
-            <h3 className="" key={portfolio.id}>
+            <h3 className="">
                 {/* <u>{portfolio.portfolio_name}</u> */}
+                <Watchlist data={watchlist}/>
             </h3>  
         </div>    
     )
