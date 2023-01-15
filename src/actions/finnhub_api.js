74a6f92de92d4070840083cf05dc7e8ba051c3ef
Jennifer Beaver <jen.lee.beaver@gmail.com>
2023-01-15 10:17:51 -0800

    update .env requirements

--- src/actions/finnhub_api.js
+++ src/actions/finnhub_api.js
@@ -1,10 +1,11 @@
 //will have to hide key in .env file and git-ignore
-
+require('dotenv').config();
 const finnhub = require('finnhub');
 
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = process.env.REACT_APP_FINNHUB_API_KEY;
+api_key.apiKey = process.env.REACT_APP_FINNHUB_API_KEY
 
-const finnhubClient = new finnhub.DefaultApi();
+
+const finnhubClient = new finnhub.DefaultApi()
 
 export default finnhubClient;
